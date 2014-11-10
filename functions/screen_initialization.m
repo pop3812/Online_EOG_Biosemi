20c12e9aef5d35efc510c32d7e5b77f3db7d9785
KwangRyeolLee <yakwang@kaist.ac.kr>
2014-11-10 16:54:52 +0900

    [M] Minor Changes
    Minor Changes.

--- functions/screen_initialization.m
+++ functions/screen_initialization.m
@@ -12,12 +12,19 @@ if(n_monitor > 1) % Double monitor
     stim_window = stimulus_window();
     set(stim_window.window, 'Units', 'pixels');
     set(stim_window.axes, 'Units', 'pixels');
+    hold(stim_window.axes, 'on');
     
     set(stim_window.window, 'Position', [scrsz(3)+1 1 1280 1024]);
     set(stim_window.axes, 'Position', [1 1 1280 1024]);
     
+    set(stim_window.axes, 'XTick', [], 'YTick', []);
+    whitebg(stim_window.window, 'black'); % Change the background color
+
+    
     scatter(stim_window.axes, 0, 0, 300, 'x', 'LineWidth', 3, ...
-        'MarkerEdgeColor', 'k');
+        'MarkerEdgeColor', 'w');
+    
+
     
     params.DoubleMonitor = 1;
     
