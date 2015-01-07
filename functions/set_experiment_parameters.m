58df5109f6c6f8463ff72ad9cc5ae2d462288f1e
KwangRyeolLee <yakwang@kaist.ac.kr>
2015-01-07 17:59:05 +0900

    [A] Add Voice Instructions
    I added voice instructions.

--- functions/set_experiment_parameters.m
+++ functions/set_experiment_parameters.m
@@ -23,7 +23,7 @@ params.DelayTime = 0.25; % in sec
 params.BufferTime = 15; % in sec
 params.CalibrationTime = 4; % in sec
 params.DataAcquisitionTime = 8; % in sec
-params.ResultShowTime = 1; % in sec
+params.ResultShowTime = 5; % in sec
 
 % pre-processing parameters
 params.blink_calibration_time = 5; % in sec
